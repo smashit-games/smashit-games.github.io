8a7d1940883b77432f42321ec58060f242e83911
Luke Hewitt <lukehewitt@smashit.games>
2024-11-01 12:25:47 +1000

    tryin to fix routing issue

--- src/components/ExternalRedirect.tsx
+++ src/components/ExternalRedirect.tsx
@@ -1,17 +1,15 @@
 ﻿// src/components/ExternalRedirect.tsx
+
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 interface ExternalRedirectProps {
   to: string;
 }
 
 const ExternalRedirect: React.FC<ExternalRedirectProps> = ({ to }) => {
-  const navigate = useNavigate();
-
   useEffect(() => {
-    window.location.href = to; // Redirects the browser to the specified URL
-  }, [to, navigate]);
+    window.location.href = to;
+  }, [to]);
 
   return null;
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,28 @@
+// src/index.tsx
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import PrivacyPolicy from './components/PrivacyPolicy';
 import FSPressKit from './components/FSPressKit';
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
+import ExternalRedirect from './components/ExternalRedirect'
 
 const rootElement = document.getElementById('root') as HTMLElement;
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         <Route path="/fspresskit" element={<FSPressKit />} />
         <Route
           path="/discord"
-          element={<Navigate to="https://discord.gg/84zvK3y3Cs" replace />}
+          element={<ExternalRedirect to="https://discord.gg/84zvK3y3Cs" />}
         />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
