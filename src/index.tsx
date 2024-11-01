88aa2e8b863ee51276cd871c08106fa84410ff0d
Luke Hewitt <lukehewitt@smashit.games>
2024-11-01 11:57:10 +1000

    hash router and 404 fix?

--- src/index.tsx
+++ src/index.tsx
@@ -3,24 +3,23 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import PrivacyPolicy from './components/PrivacyPolicy';
 import FSPressKit from './components/FSPressKit';
-import ExternalRedirect from './components/ExternalRedirect';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './index.css';
 
 const rootElement = document.getElementById('root') as HTMLElement;
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         <Route path="/fspresskit" element={<FSPressKit />} />
         <Route
           path="/discord"
-          element={<ExternalRedirect to="https://discord.gg/84zvK3y3Cs" />}
+          element={<Navigate to="https://discord.gg/84zvK3y3Cs" replace />}
         />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
