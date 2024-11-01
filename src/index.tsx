e0fe50765915cb9f20556f8254bcc79b51c43b16
Luke Hewitt <lukehewitt@smashit.games>
2024-11-01 12:47:59 +1000

    tryin this thing

--- src/index.tsx
+++ src/index.tsx
@@ -7,19 +7,19 @@ import PrivacyPolicy from './components/PrivacyPolicy';
 import FSPressKit from './components/FSPressKit';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
-import ExternalRedirect from './components/ExternalRedirect'
+import ExternalRedirect from './components/ExternalRedirect';
 
 const rootElement = document.getElementById('root') as HTMLElement;
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-        <Route path="/fspresskit" element={<FSPressKit />} />
+        <Route path="" element={<App />} />
+        <Route path="privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="fspresskit" element={<FSPressKit />} />
         <Route
-          path="/discord"
+          path="discord"
           element={<ExternalRedirect to="https://discord.gg/84zvK3y3Cs" />}
         />
       </Routes>
